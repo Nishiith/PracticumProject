250525bf6d06877164cdbc52e7cd13eb10e9971e
Nishat Tasneem <nishatasneem188@gmail.com>
2022-11-09 01:44:13 +0600

    first commit

---
+++ resources/views/backend/fixed/sidebar.blade.php
@@ -0,0 +1,63 @@
+<div class="nano-content">
+                <ul>
+                    <div class="logo"><a href="index.html">
+                            <!-- <img src="images/logo.png" alt="" /> --><span>Focus</span></a></div>
+                    <li class="label">Main</li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-home"></i> Dashboard <span
+                                class="badge badge-primary">2</span> <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                        <ul>
+                            <li><a href="index.html">Home</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="label">Apps</li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-bar-chart-alt"></i> Add Items <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                      
+                    </li>
+                    <li><a href="app-event-calender.html"><i class="ti-calendar"></i> Stocks </a></li>
+                    
+                   
+                    <li><a class="sidebar-sub-toggle"><i class="ti-layout"></i> Supplied Items <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                       
+                    </li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-panel"></i> Report <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                       
+                    </li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-layout-grid4-alt"></i> Warehouse <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                       
+                    </li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-heart"></i> Back-up <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                       
+                    </li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-map"></i> Permission <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                       
+                    </li>
+                    <li class="label">Form</li>
+                    <li><a href="form-basic.html"><i class="ti-view-list-alt"></i> Basic Form </a></li>
+                    <li class="label">Extra</li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-files"></i> Invoice <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                        <ul>
+                            <li><a href="invoice.html">Basic</a></li>
+                            <li><a href="invoice-editable.html">Editable</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-target"></i> Pages <span
+                                class="sidebar-collapse-icon ti-angle-down"></span></a>
+                        <ul>
+                            <li><a href="page-login.html">Login</a></li>
+                            <li><a href="page-register.html">Register</a></li>
+                            <li><a href="page-reset-password.html">Forgot password</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="../documentation/index.html"><i class="ti-file"></i> Documentation</a></li>
+                    <li><a><i class="ti-close"></i> Logout</a></li>
+                </ul>
+            </div>
